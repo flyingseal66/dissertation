66195943913f34b9a6ec10df5952d2e73f3913a0
flyingseal66 <2576209723>
2019-06-28 08:59:32 +0100

    add quicksortShortSingle file

--- dissertation/main.cpp
+++ dissertation/main.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 #include <ctime>
+#include <random>
+#include <thread>
 #include "quicksortShortSingle.h"
 
+const int NMIN = 1000000;
+const int NMAX = 10000000;
+const int NAVG = 2;
 using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Program started!" << endl;
 
-    int len = 100000000;
-    int  *data = new int[len];
-    clock_t startTime, endTime;
-
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < len; i++)
-    {
-        data[i] =rand();
-    }
-
-    srand((unsigned)time(NULL));
-    /*for (int i = 0; i < len; i++)
-    {
-        data[i] = rand();
-    }*/
-
-
-    startTime = clock();// start time
-    quickSort(data, 0, len - 1);
-    //sort(data, data+ len);
-    endTime = clock();//end time
-    cout << "The quicksortShort2 run time is: " << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s " << endl;
-    //cout << "total cnt" << cnt << endl;
-    //std::copy(data, data + (sizeof(data) / sizeof(*data)), std::ostream_iterator<int>(std::cout, " "));
-    for (int i = 1; i < len; i++) {
-        if (data[i] < data[i - 1]) {
-            std::cout << "Wrong order is found in Radix sort MSD:" << i << " " << data[i] << " " << data[i - 1] << std::endl;
+    for (int i = NMIN; i < NMAX; i *= 10) {
+        int  *data = new int[i];
+        auto start = chrono::steady_clock::now(); // start time
+        quickSort(data, 0, i - 1);
+        auto end = chrono::steady_clock::now();
+        cout << chrono::duration <double, milli>(end - start).count() << " ms" << endl;
+        for (int j = 1; j < i; j++) {
+            if (data[i] < data[i - 1]) {
+                std::cout << "Wrong order is found in Radix sort MSD:" << i << " " << data[i] << " " << data[i - 1] << std::endl;
+            }
         }
     }
-
     return 0;
+}
+
+void initData(int *data, int size) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(1, 32767);
+    for (int i = 0; i < size; ++i) {
+        data[i] = dist(mt);
+    }
 }
